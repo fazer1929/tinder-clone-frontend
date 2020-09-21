57c1bed12a01b5d529455d4b2bd0c30611ed7e56
fazer1929 <abhishekagrawal8888@gmail.com>
2020-09-21 19:45:23 +0530

    Updates

---
+++ precache-manifest.268ee3040d2b9745bd84189998345dca.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "97e49e7726166d1400c0620c3b787754",
+    "url": "/tinder-clone-frontend/index.html"
+  },
+  {
+    "revision": "9ae98710d9f870a4ac55",
+    "url": "/tinder-clone-frontend/static/css/main.d25486f6.chunk.css"
+  },
+  {
+    "revision": "001a34fdfabd3bcdcf56",
+    "url": "/tinder-clone-frontend/static/js/2.8bd56941.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/tinder-clone-frontend/static/js/2.8bd56941.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9ae98710d9f870a4ac55",
+    "url": "/tinder-clone-frontend/static/js/main.0058c9ae.chunk.js"
+  },
+  {
+    "revision": "8d3febef5a174e310a5f",
+    "url": "/tinder-clone-frontend/static/js/runtime-main.d36eb67a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/tinder-clone-frontend/static/media/logo.5d5d9eef.svg"
+  }
+]);
